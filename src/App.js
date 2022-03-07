f5c3e3169471c26c479d95b7bf9f53a1d028f272
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-07 11:04:10 +1100

    Update App.js
    Create function to toggle squares from App.js through id property that comes from Box.js
    Set the state from previous squares, creating a new array
    Then, loop through every object in the array and define current square depending on the position inside of the array
    If the current square id equals the id that is given by the square, then a new variable is created spreading  values from current square, but changing the on to the opposite value of on on the current square.
    Then, we push the update square into the new squares array.
    If the id is NOT equal to the id given by the square, then we push the currentSquare into the new squares array.
    Return new array.
    When rendering the Box component, we need to make the id accessible by declaring it again with another name(id).
    We add on the toggle function to toggle when clicking on the box component.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import "./App.css";
 import boxes from "./Boxes";
 import Box from "./Box";
 
-function App(props) {
+export default function App() {
   const [squares, setSquares] = useState(boxes);
 
+  function toggle(id) {
+    setSquares((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
+  }
+
   const squareElements = squares.map((square) => (
-    <Box key={square.id} on={square.on} />
+    <Box key={square.id} id={square.id} on={square.on} toggle={toggle} />
   ));
 
   return <main>{squareElements}</main>;
 }
-
-export default App;
