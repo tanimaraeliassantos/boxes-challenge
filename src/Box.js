fe011edfa53988d0587b2f233383a66a71677748
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-07 11:06:10 +1100

    Update Box.js
    We remove state from child component
    We add props back to declaration of style (because we removed state declaration)
    We return an onClick of div that transforms the event that was received, it grabs the props.id and then toggles it.

--- src/Box.js
+++ src/Box.js
@@ -1,12 +1,15 @@
-import React, { useState  } from "react";
+import React from "react";
 
 export default function Box(props) {
-  const [on, setOn] = useState(props.on);
   const styles = {
-    backgroundColor: on ? "#222222" : "transparent",
+    backgroundColor: props.on ? "#222222" : "transparent",
   };
-  function toggle() {
-    setOn((prevOn) => !prevOn);
-  }
-  return <div style={styles} className="box" onClick={toggle}></div>;
+
+  return (
+    <div
+      style={styles}
+      className="box"
+      onClick={() => props.toggle(props.id)}
+    ></div>
+  );
 }
